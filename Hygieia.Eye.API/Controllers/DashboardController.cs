14095566b69fd229eca32d149bd6a2a9fc093f2b
Chaminda Kodithuwakku <chaminda.k@evicio.com>
2021-11-03 18:22:10 +0530

    hygiya

---
+++ Hygieia.Eye.API/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿// NOTE: please use this controller to do the dummmy endpoints 
+namespace Hygieia.Eye.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController : ControllerBase
+    {
+
+        private readonly ILogger<DashboardController> _logger;
+        private static int crowd = 0;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetCrowd")]
+        public int GetCrowd()
+        {
+            crowd = crowd + 1;
+            return crowd;
+        }
+    }
+}
